0e14cebc3d14a738089ef13c3b143d00a2a7db17
Victor Queiroz <victorcqueirozg@gmail.com>
2014-04-25 08:00:51 -0300

    v1.0: Module finished.

--- src/angular-paginator.js
+++ src/angular-paginator.js
@@ -1,5 +1,70 @@
-angular.module('paginator', ['ng']).
-provider('$paginator', [function () {
-  this.$get = [function paginatorFactory() {
-  }];
-}])
+'use strict';
+
+var paginatorModule = angular.module('paginator', []);
+
+paginatorModule.factory('Paginator', function() {
+
+  var paginator = {};
+
+  paginator.new = function(perPage) {
+
+    perPage = perPage === undefined ? 5 : perPage;
+
+    var paginator = {
+      pages : {integer: 1, toArray: function (){
+          var array = [];
+
+          for(var i=0; i < paginator.pages.integer; i++) {
+            array.push(i);
+          }
+
+          return array;
+        }
+      },
+      perPage: perPage,
+      page: 0
+    };
+
+    paginator.prevPage = function() {
+      if (paginator.page > 0) {
+        paginator.page -= 1;
+      }
+    };
+
+    paginator.nextPage = function() {
+      if (paginator.page < paginator.pages.integer - 1) {
+        paginator.page += 1;
+      }
+    };
+
+    paginator.toPageId = function(id) {
+      if (id >= 0 && id <= paginator.pages.integer - 1) {
+        paginator.page = id;
+      }
+    };
+
+    return paginator;
+  };
+
+  return paginator;
+});
+
+paginatorModule.filter('startFrom', function() {
+  return function(input, start) {
+    if (input === undefined) {
+      return input;
+    } else {
+      return input.slice(+start);
+    }
+  };
+});
+
+paginatorModule.filter('range', function() {
+  return function(input, total) {
+    total = parseInt(total);
+    for (var i = 0; i < total; i++) {
+      input.push(i);
+    }
+    return input;
+  };
+});
--- test/js/controllers.js
+++ test/js/controllers.js
@@ -1,8 +1,9 @@
-app.controller('TableCtrl', ['$scope', '$paginator', function ($scope, $paginator) {
+app.controller('TableCtrl', ['$scope', 'Paginator', function ($scope, Paginator) {
   var users = $scope.users = [];
 
   for(var i=0; i < 300; i++) {
     users.push({
+      id: i + 1,
       name: Faker.Name.findName(),
       email: Faker.Internet.email(),
       gender: Faker.Name.gender(),
@@ -10,4 +11,16 @@ app.controller('TableCtrl', ['$scope', '$paginator', function ($scope, $paginato
       description: Faker.Lorem.paragraph()
     });
   }
+
+  $scope.users.paginator = Paginator.new();
+  $scope.users.paginator.pages.integer = Math.ceil($scope.users.length/$scope.users.paginator.perPage);
+
+  var messages = $scope.messages = [];
+
+  for(var i=0; i < 100; i++) {
+    messages.push({id: i + 1, message: Faker.Lorem.paragraphs(2)});
+  }
+
+  $scope.messages.paginator = Paginator.new();
+  $scope.messages.paginator.pages.integer = Math.ceil($scope.messages.length/$scope.messages.paginator.perPage);
 }]);
